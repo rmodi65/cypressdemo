53e6002ebe819ff876ae650145461de001c6be59
rohit <rmodi65@gmail.com>
2022-03-14 11:27:27 +0530

    windowhandle updated

--- cypress/integration/1-getting-started/handleWebTable.js
+++ cypress/integration/1-getting-started/handleWebTable.js
@@ -2,7 +2,7 @@
 describe('this is webtable demo scenario', () => {
 
     let company='Srestha Finvest Ltd.'
-    xit('this is first test case of webtable demo', () => {
+    it('this is first test case of webtable demo', () => {
        cy.visit('https://money.rediff.com/companies/results')
        cy.get(".dataTable tr td:nth-child(1)").each(($el,index,arr)=>{
            if($el.text().includes(company)){cy.wrap(index).as('position'); return false}
@@ -19,7 +19,15 @@ describe('this is webtable demo scenario', () => {
         cy.get('.txt_srch').type('Coforge ltd').type('{downArrow}').type('{enter}')
         cy.title().should('include','COFORGE')
         //getcurrent price
-        cy.get('')
+        cy.get('#ltpid').invoke('text').then(function(value){
+            cy.log('coforge current price is '+value)
+        })
+        cy.get('#FiftyTwoHighlow').invoke('text').then(function(value){
+            cy.log('coforge 52 week high price is '+value.trim().split('-')[0])
+            cy.log('coforge 52 week low price is '+value.trim().split('-')[1])
+
+        })
+       
     })
     
 });
